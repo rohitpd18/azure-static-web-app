4443c8d66013dcaf0fcf30305ab634c370b2d782
rohitpd18 <91771522+rohitpd18@users.noreply.github.com>
2022-11-20 20:35:53 +0530

    test route added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Home from "./Home";
 import PageNotFound from "./PageNotFound";
+import Test from "./Test";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Home/>} />
+        <Route exact path="/test" element={<Test/>} />
         <Route path="*" element={<PageNotFound/>} />
       </Routes>
     </BrowserRouter>
