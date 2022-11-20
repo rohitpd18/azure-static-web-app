594b613b401542fe6c1772a2e422c19653099a5c
rohitpd18 <91771522+rohitpd18@users.noreply.github.com>
2022-11-20 20:37:38 +0530

    test

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Home = () => {
   return (
     <div>
       this is home
+      <Link to="/test">test</Link>
     </div>
   )
 }
